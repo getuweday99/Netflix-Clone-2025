3335429bf51141aa5cdcd617b6023df14fb12c3b
getuweday99 <getuweday99@gmail.com>
2025-02-11 19:01:55 +0300

    axios

--- netflix-clone/src/pages/Home/Home.jsx
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import "./Home.css"
+import "./Home.css";
 import Header from "../../components/Header/Header";
+import Banner from "../../components/Banner/Banner";
 import Footer from "../../components/Footer/Footer";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <Banner />
       <Footer />
     </>
   );
