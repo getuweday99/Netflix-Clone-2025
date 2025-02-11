97d825500acab1e75e9881bdbfecd7682c2ac7df
getuweday99 <getuweday99@gmail.com>
2025-02-12 00:18:40 +0300

    rows and rowlists

--- netflix-clone/src/pages/Home/Home.jsx
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Home.css";
 import Header from "../../components/Header/Header";
 import Banner from "../../components/Banner/Banner";
+import RowList from "../../components/Row/RowList/RowList";
 import Footer from "../../components/Footer/Footer";
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
     <>
       <Header />
       <Banner />
+      <RowList />
       <Footer />
     </>
   );
